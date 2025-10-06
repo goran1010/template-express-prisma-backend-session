55e49ac808827bbaa9417a6f027f12fa9853c190
Goran Jovic <goran1010jovic@gmail.com>
2025-10-06 22:02:53 +0200

    refactor: split index.js into index.js and app.js

---
+++ app.js
@@ -0,0 +1,41 @@
+import express from "express";
+import passport from "./auth/passport.js";
+import "dotenv/config";
+const app = express();
+import sessionMiddleware from "./auth/sessionMiddleware.js";
+import cors from "cors";
+import isNotLoggedIn from "./auth/isNotLoggedIn.js";
+import indexRouter from "./routes/indexRouter.js";
+
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:5173",
+  }),
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+import path from "node:path";
+
+const assetsPath = path.join(import.meta.dirname, "public");
+app.use(express.static(assetsPath));
+
+app.use(sessionMiddleware);
+app.use(passport.session());
+
+app.use("/", isNotLoggedIn, indexRouter);
+
+app.use((req, res) => {
+  res.status(404).json("No resource found");
+});
+
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  // eslint-disable-next-line no-console
+  console.error(err);
+  res.status(500).json(err.message || "Error 500: Internal Server Error");
+});
+
+export default app;
--- index.js
+++ index.js
@@ -1,43 +1,10 @@
-import express from "express";
-import passport from "./auth/passport.js";
 import "dotenv/config";
-const app = express();
+import app from "./app.js";
+
 const PORT = process.env.PORT || 3000;
-import sessionMiddleware from "./auth/sessionMiddleware.js";
-import cors from "cors";
-
-app.use(cors());
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-import path from "node:path";
-
-const assetsPath = path.join(import.meta.dirname, "public");
-app.use(express.static(assetsPath));
-
-import apiRouter from "./routes/apiRouter.js";
-
-app.use(sessionMiddleware);
-app.use(passport.session());
-
-app.use("/api", apiRouter);
-
-app.use((req, res) => {
-  res.status(404).json("No resource found");
-});
-
-// eslint-disable-next-line no-unused-vars
-app.use((err, req, res, next) => {
-  // eslint-disable-next-line no-console
-  console.error(err);
-  res.status(500).json(err.message || "Error 500: Internal Server Error");
-});
 
 app.listen(PORT, (error) => {
-  if (error) {
-    throw error;
-  }
+  if (error) throw error;
   // eslint-disable-next-line no-console
   console.log(`App started at port: ${PORT}`);
 });
